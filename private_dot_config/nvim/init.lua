75dcd42e0e86313da72f320d2da8bd2ecff97c12
Michael Melancon <MMelancon@DentalIntel.com>
2024-08-14 16:45:59 -0500

    Added nvim config

---
+++ private_dot_config/nvim/init.lua
@@ -0,0 +1,15 @@
+-- See `:help mapleader`
+--  NOTE: Must happen before plugins are loaded (otherwise wrong leader will be used)
+vim.g.mapleader = " "
+vim.g.maplocalleader = " "
+
+vim.g.have_nerd_font = true
+
+require("opts")
+require("keymaps")
+require("autocmds")
+
+require("lazy")
+-- require("rocks")
+
+-- vim: ts=2 sts=2 sw=2 et

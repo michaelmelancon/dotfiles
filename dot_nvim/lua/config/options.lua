7771582985f2edaf82c2e8c1e370ec08e4da540a
Michael Melancon <MMelancon@DentalIntel.com>
2024-08-26 20:23:35 -0500

    disabled optional language providers for neovim.

--- dot_nvim/lua/config/options.lua
+++ dot_nvim/lua/config/options.lua
@@ -2,6 +2,13 @@
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
 vim.g.mapleader = ";"
 vim.g.maplocalleader = "\\"
+
+-- Disable Language providers
+vim.g.loaded_node_provider = 0
+vim.g.loaded_perl_provider = 0
+vim.g.loaded_python3_provider = 0
+vim.g.loaded_ruby_provider = 0
+
 vim.opt.scrolloff = 16
 vim.opt.inccommand = "split"
 

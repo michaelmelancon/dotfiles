7c6205428e38029013f556f8bf15cb34542d4c13
Michael Melancon <michaelmelancon@gmail.com>
2024-09-04 06:49:37 -0500

    reset leader

--- dot_nvim/lua/config/options.lua
+++ dot_nvim/lua/config/options.lua
@@ -1,7 +1,5 @@
 -- Options are automatically loaded before lazy.nvim startup
 -- Default options that are always set: https://github.com/LazyVim/LazyVim/blob/main/lua/lazyvim/config/options.lua
-vim.g.mapleader = ";"
-vim.g.maplocalleader = "\\"
 
 -- Disable Language providers
 vim.g.loaded_node_provider = 0
@@ -10,7 +8,6 @@ vim.g.loaded_python3_provider = 0
 vim.g.loaded_ruby_provider = 0
 
 vim.opt.scrolloff = 16
-vim.opt.inccommand = "split"
 
 vim.filetype.add({
   extension = {
@@ -21,3 +18,4 @@ vim.filetype.add({
 if vim.fn.has("win32") == 1 then
   vim.opt.shell = "pwsh.exe"
 end
+

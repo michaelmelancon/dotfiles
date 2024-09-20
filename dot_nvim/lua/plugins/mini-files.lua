6876a473cc6499310abe329de3519eb684d3a08c
Michael Melancon <michaelmelancon@gmail.com>
2024-09-19 19:27:20 -0500

    Switched neovim to use mini.files instead of neo-tree.

--- dot_nvim/lua/plugins/mini-files.lua
+++ dot_nvim/lua/plugins/mini-files.lua
@@ -5,10 +5,26 @@ return {
       windows = {
         preview = true,
         width_preview = 100,
-        width_focus = 40,
+        width_focus = 30,
       },
       options = {
         use_as_default_explorer = true,
+      },
+    },
+    keys = {
+      {
+        "<leader>e",
+        function()
+          require("mini.files").open(vim.api.nvim_buf_get_name(0), true)
+        end,
+        desc = "Open mini.files (Directory of Current File)",
+      },
+      {
+        "<leader>E",
+        function()
+          require("mini.files").open(vim.uv.cwd(), true)
+        end,
+        desc = "Open mini.files (cwd)",
       },
     },
   },
